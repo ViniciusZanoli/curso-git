1090cf738ca6a4cf1a678544d9c628aa49881429
Vinicius Zanoli <viniciusdasilva.92@hotmail.com>
2019-01-19 05:43:29 -0200

    Projeto Curso01

---
+++ BancoVinicius/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoVinicius
+{
+    class Conta
+    {
+        const int Taxa = 5;
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            depositar(depositoInicial);
+        }
+
+        public double depositar(double valor)
+        {
+            Saldo += valor;
+            return Saldo;
+        }
+
+        public double sacar(double valor)
+        {
+            Saldo -= valor + Taxa;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta: \n"
+                + "Conta " + Numero + ", "
+                + "Titular: " + Titular + ", "
+                + "Saldo: $ " + Saldo.ToString("F2");
+        }
+
+    }
+}
---
+++ BancoVinicius/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoVinicius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcao = char.Parse(Console.ReadLine());            
+
+            if (opcao == 's')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular, depositoInicial);                
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para deposito: ");
+            conta.depositar(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+
+            Console.Write("Entre com um valor para saque: ");
+            conta.sacar(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine(conta);            
+
+            Console.ReadLine();
+            
+        }
+    }
+}
